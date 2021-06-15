1503bc9d14d2ce1722a402305c4ee41f84e367d0
syedansar <syedansar6666@gmail.com>
2021-06-15 13:22:00 +0530

    Review - project 03

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,65 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setIndex(() => {
+      let newIndex = index + 1;
+      return CheckNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex(() => {
+      let newIndex = index + 1;
+      return CheckNumber(newIndex);
+    });
+  };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(CheckNumber(randomNumber));
+  };
+
+  const CheckNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <div className="author">{name}</div>
+      <p className="job">{job}</p>
+      <div className="info">{text}</div>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={randomPerson}>
+        Surprise
+      </button>
+    </article>
+  );
 };
 
 export default Review;
